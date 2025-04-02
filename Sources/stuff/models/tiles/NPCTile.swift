27005a8fdc29a7473f9f354834a94121d4cb22ba
OrigamiKing3612 <115757805+OrigamiKing3612@users.noreply.github.com>
2025-04-01 20:08:10 -0500

    Use a json key

--- Sources/stuff/models/tiles/NPCTile.swift
+++ Sources/stuff/models/tiles/NPCTile.swift
@@ -2,13 +2,12 @@ import Foundation
 
 struct NPCTile: Codable, Equatable {
 	let id: UUID
-	let villageID: UUID
+	let startingVillageNPC: Bool = true
 	let npcID: UUID
 
 	init(id: UUID = UUID(), npc: NPC) {
 		self.id = id
 		self.npcID = npc.id
-		self.villageID = UUID() // Will be overwriten in game
 	}
 }
 
@@ -17,19 +16,18 @@ extension NPCTile {
 		var container = encoder.container(keyedBy: CodingKeys.self)
 		try container.encode(id, forKey: .id)
 		try container.encode(npcID, forKey: .npcID)
-		try container.encode(villageID, forKey: .villageID)
+		try container.encode(startingVillageNPC, forKey: .startingVillageNPC)
 	}
 
 	enum CodingKeys: CodingKey {
 		case id
 		case npcID
-		case villageID
+		case startingVillageNPC
 	}
 
 	init(from decoder: any Decoder) throws {
 		let container = try decoder.container(keyedBy: CodingKeys.self)
 		self.id = try container.decode(UUID.self, forKey: .id)
 		self.npcID = try container.decode(UUID.self, forKey: .npcID)
-		self.villageID = try container.decode(UUID.self, forKey: .villageID)
 	}
 }
